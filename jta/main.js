74ca4665907ec434910f4a44475dad51e1bebd70
John Albritton <jtareb1@att.net>
2015-01-26 13:35:36 -0500

    Fixed file structure

---
+++ jta/main.js
@@ -0,0 +1,18 @@
+function fizz () {
+	var startnum = 1;
+	var currentnum;
+	var endnum = 100;
+
+	for (i = 0; i < endnum; i++ ) {
+		currentnum = startnum[i];
+			If (currentnum = 3):
+			  
+	}
+
+
+
+
+
+
+
+}
